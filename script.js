9e54c1d63154eb791bee52591b645571053516fa
Anirudh Dwivedi <141205572+Anirudh0202@users.noreply.github.com>
2024-07-03 22:20:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const typingText=document.querySelector('.typing text p')
+const input=document.querySelector('.wrapper .input field ')
+const time=document.querySelector('.time span b')
+const typingText=document.querySelector('.typing text p')
+const typingText=document.querySelector('.typing text p')
+const typingText=document.querySelector('.typing text p')
+const typingText=document.querySelector('.typing text p')
